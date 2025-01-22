9bd4ca74c88f36745d0499ea6b9cac42921e2713
Lee Yeseung <149143116+lys0611@users.noreply.github.com>
2025-01-22 23:57:30 +0900

    Update setup_db.sh

--- ApiServer/setup_db.sh
+++ ApiServer/setup_db.sh
@@ -3,7 +3,7 @@
 # 환경변수 설정(필요한 경우)
 # 실제 환경에 맞게 수정 가능
 MYSQL_USER="${MYSQL_USER:-admin}"
-MYSQL_PASS="${MYSQL_PASS:-admin123}"
+MYSQL_PASS="${MYSQL_PASS:-admin1234}"
 
 # MySQL 포트 체크 (선택사항)
 if ! timeout 3 bash -c "</dev/tcp/$MYSQL_HOST/3306" 2>/dev/null; then
