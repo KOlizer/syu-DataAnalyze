6e8c51a49ac1dd8483b41faac1f5eb27c53538f5
cjm0423 <87014584+cjm0423@users.noreply.github.com>
2025-01-31 13:18:16 +0900

    Update producer.py

--- Kafka_Connect_VM/producer.py
+++ Kafka_Connect_VM/producer.py
@@ -3,7 +3,7 @@ import time
 
 
 # 프로듀서 인스턴스 생성
-p = Producer({'bootstrap.servers': KAFKA_BOOTSTRAP_SERVERS})
+p = Producer({'bootstrap.servers': ${KAFKA_BOOTSTRAP_SERVERS}})
 
 # 메시지 전송 완료 시 호출될 콜백 함수
 def delivery_callback(err, msg):
@@ -15,7 +15,7 @@ def delivery_callback(err, msg):
 # 5개의 메시지를 1초 간격으로 전송
 for i in range(5):
     message = f"Message {i}"
-    p.produce(topic, message.encode('utf-8'), callback=delivery_callback)
+    p.produce(${KAFKA_PYTHON_TOPIC}, message.encode('utf-8'), callback=delivery_callback)
     p.poll(0)  # 이벤트 큐를 폴링하여 콜백이 실행되도록
     time.sleep(1)
 
